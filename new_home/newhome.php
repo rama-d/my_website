3e6acb8399f3589f4db9295499bc340b02c5b8d0
rama-d <57993727+rama-d@users.noreply.github.com>
2022-08-15 12:38:10 +0300

    Add files via upload

---
+++ new_home/newhome.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New home | Rama sultan</title>
+</head>
+<link rel="stylesheet" href="newhome.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+<header class="header">
+        <a href="" class="logo">Rama Sultan</a>
+        <input class="menu-btn" type="checkbox" id="menu-btn">
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+          <li><a href="index.php" target="">HOME</a></li>
+          <li><a href="../about/about.php">ABOUT</a></li>
+          <li><a href="../career/career.php">CAREER</a></li>
+          <li><a href="../service/service.php">SERVICE</a></li>
+          <!-- <div class="header-btn"> -->
+           <a href="../contact/contact.php"> <button name="submit" type="button"  formaction="../contact/contact.html" id="header-button" data-submit="..sending">CONTACT</button></a>
+          <!-- </div> -->
+        </ul>
+      </header>  
+
+      <div class="slideshow">
+        <div class="form-side">
+        <form action="connect.php" method="post">
+        <h3>Quick Contact</h3>
+        <h4>Contact us today, and get reply with in 24 hours!</h4>
+        <fieldset>
+          <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+        </fieldset>
+        <fieldset>
+          <input placeholder="Your Email Address" type="email" tabindex="2" required>
+        </fieldset>
+        <fieldset>
+          <input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+        </fieldset>
+        <fieldset>
+          <input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required>
+        </fieldset>
+        <fieldset>
+          <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+        </fieldset>
+        <fieldset>
+         <a href=""> <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button></a>
+        </fieldset>
+      </form>
+        </div>
+        <div class="content-side">
+          <h1>my website</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo fuga pariatur fugit voluptate, consequatur maiores odit vel commodi eum dolorem minima aspernatur aut quia tempora quam ipsum hic sunt tenetur?</p>
+
+          <div class="desc-button">
+          <button class="first-button" type="button">Call me</button>
+          <button class="second-button" type="button">About me</button>
+         </div>
+        </div>
+      </div>
+
+      <div class="wrapper">
+        <div class="left-wrapp">
+          <h1>this is my site</h1>
+          <!-- <hr id="heading-hr"> -->
+          <div class="heading-line"></div>
+          <h2>my second heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur suscipit molestias ipsum asperiores. Quae at, tempora facere dolores officiis, inventore possimus itaque architecto deserunt laboriosam non ipsum, distinctio reprehenderit. Voluptatum!</p>
+        </div>
+        <div class="right-wrapp">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio excepturi nesciunt repellendus id vero, ab architecto qui amet, voluptatum consequatur soluta dolore nam ipsam saepe nostrum ducimus, nihil doloremque voluptate?</p><br>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio excepturi nesciunt repellendus id vero, ab architecto qui amet, voluptatum consequatur soluta dolore nam ipsam saepe nostrum ducimus, nihil doloremque voluptate?</p>
+        </div>
+
+      </div>
+
+      <section class="parent-div">
+         <div class="my-class">
+         <i class="fa fa-cogs" aria-hidden="true"></i>
+          <h2>my heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laborum libero esse repellat hic mollitia laudantium quia voluptas ipsum sapiente maiores repellendus,</p>
+         </div>
+         <div class="my-class" >
+         <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+          <h2>my heading</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, unde ut. Error officiis veniam nam deserunt eos iure sed! Minima reprehenderit,</p>
+         </div>
+         <div class="my-class" >
+         <i class="fa fa-wifi" aria-hidden="true"></i>
+          <h2>my heading</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis placeat similique, ut hic labore, impedit ipsa nobis culpa perferendis earum et id fuga reiciendis dolor,</p>
+         </div>
+         <div class="my-class" >
+         <i class="fa fa-bolt" aria-hidden="true"></i>
+          <h2>my heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus natus officiis sit beatae eum esse assumenda deserunt rem impedit dignissimos doloremque, similique, </p>
+         </div>
+      </section>
+
+      <article class="description-part">
+      <div class="part">
+       <div> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+         <div> <h3>storage</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quis recusandae harum velit sint. Voluptatum impedit, esse minus,</p></div>
+        </div>
+        <div class="part">
+       <div> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+         <div> <h3>storage</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quis recusandae harum velit sint. Voluptatum impedit, esse minus,</p></div>
+        </div>
+        <div class="part">
+       <div> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+         <div> <h3>storage</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quis recusandae harum velit sint. Voluptatum impedit, esse minus,</p></div>
+        </div>
+        <div class="part">
+       <div> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+         <div> <h3>storage</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quis recusandae harum velit sint. Voluptatum impedit, esse minus,</p></div>
+        </div>
+      </article>
+
+      <div class="portfolio-hearder">
+        <h2>Portfolio heading</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime tempore enim, velit odit labore nisi, aut illum porro</p>
+      </div>
+
+      <section class="portfolio">
+        <div class="img-part">
+          <img src="../images/image3.jpg" alt="">
+        </div>
+        <div class="content-part">
+        <i class="fa fa-line-chart" aria-hidden="true"></i>
+          <h2>heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolor accusamus nobis. Eveniet voluptatibus quisquam aliquid illo, perferendis at!</p>
+        </div>
+        <div class="img-part">
+          <img src="../images/image6.jpg" alt="">
+        </div>
+        <!-- second row -->
+        <div class="content-part">
+        <i class="fa fa-bar-chart" aria-hidden="true"></i>
+        <h2>heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolor accusamus nobis. Eveniet voluptatibus quisquam aliquid illo, perferendis at!</p>
+        </div>
+        <div class="img-part">
+          <img src="../images/image5.jpg" alt="">
+        </div>
+        <div class="content-part">
+        <i class="fa fa-pie-chart" aria-hidden="true"></i>
+        <h2>heading</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolor accusamus nobis. Eveniet voluptatibus quisquam aliquid illo, perferendis at!</p>
+        </div>
+      </section>
+
+      <article class="about">
+        <div class="left-side">
+          <img src="../images/profile.jpg" alt="">
+        </div>
+        <div class="right-side">
+          <h1>meet rama</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae animi vitae repudiandae quas sunt laborum qui non quisquam libero, temporibus ducimus a omnis consequuntur eos minima nemo ad ab saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae animi vitae repudiandae quas sunt laborum qui non quisquam libero, temporibus ducimus a omnis consequuntur eos minima nemo ad ab saepe</p>
+        </div>
+      </article>
+
+       <!-- footer -->
+       <article class="footer">
+          <div class="footer-container">
+            <div class="footer-about">
+              <h4>About</h4><hr>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br>
+                 Ipsum corporis culpa, quod non aliquam </p>
+            </div>
+            <div class="footer-contact">
+              <h4>Contact</h4><hr>
+              <p><i class="fa fa-phone" aria-hidden="true"></i>+255 654 754 828</p>
+              <p><i class="fa fa-envelope" aria-hidden="true"></i>ramadhansultan112@gmail.com</p>
+              <p><i class="fa fa-map-marker" aria-hidden="true"></i>Alli hassan mwinyi road</p>
+            </div>
+            <div class="social-media">
+              <h4>Follow us</h4> <hr>
+              <p>
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+              </p>
+            </div> 
+          </div>
+          <hr class="my-line">
+          <div id="copyright">
+            <p>&copy; copyright RamaSultan2022</p>
+          </div>
+          
+        </article>
+</body>
+</html>
